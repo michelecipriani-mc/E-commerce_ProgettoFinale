908b67d8b5448d072b5a9efbdb8468e5e197960a
Michele Cipriani <michele.cipriani.unipegaso@gmail.com>
2025-07-28 09:40:00 +0200

    StrutturaProgetto

---
+++ src/main/java/com/smartbay/progettofinale/ProgettofinaleApplication.java
@@ -0,0 +1,13 @@
+package com.smartbay.progettofinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettofinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettofinaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartbay/progettofinale/ProgettofinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbay.progettofinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettofinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
