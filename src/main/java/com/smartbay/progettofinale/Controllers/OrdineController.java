9b718e49e47e5249ba397be9779bbb9e98bd3e39
davidelatina <davide.latina616@gmail.com>
2025-07-31 02:11:05 +0200

    Redirect a carrello: ordine fatto o saldo insufficiente

--- src/main/java/com/smartbay/progettofinale/Controllers/OrdineController.java
+++ src/main/java/com/smartbay/progettofinale/Controllers/OrdineController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 import com.smartbay.progettofinale.DTO.OrdineDTO;
 import com.smartbay.progettofinale.Security.SecurityService;
 import com.smartbay.progettofinale.Services.OrdineService;
@@ -23,9 +26,14 @@ public class OrdineController {
     }
 
     @PostMapping("/conferma")
-    public ResponseEntity<OrdineDTO> confermaOrdine() {
-        OrdineDTO ordine = ordineService.creaOrdine();
-        return ResponseEntity.ok(ordine);
+    public ModelAndView confermaOrdine(RedirectAttributes redirectAttributes) {
+        try {
+            ordineService.creaOrdine();
+            redirectAttributes.addFlashAttribute("cartSuccess", "Ordine effettuato con successo.");
+        } catch (Exception ex) {
+            redirectAttributes.addFlashAttribute("cartWarning", ex.getMessage());
+        }
+        return new ModelAndView("redirect:/carrello");
     }
 
     @GetMapping("")
