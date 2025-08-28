a765edcd9fce8374705238daf281a5053d3b6b7c
Davide Latina <davide.latina616@gmail.com>
2025-08-28 18:21:10 +0200

    documentazione pacchetto repository

--- src/main/java/com/smartbay/progettofinale/Repositories/UserRepository.java
+++ src/main/java/com/smartbay/progettofinale/Repositories/UserRepository.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Repository;
 
 import com.smartbay.progettofinale.Models.User;
 
-
+/** Repository per l'accesso e la gestione dei dati dell'entità User. */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     
+    /** Trova un utente in base al suo indirizzo email. */
     User findByEmail(String email);
+
+    /** Trova un utente in base al suo nome utente. */
     Optional<User> findByUsername(String username);
     
 }
