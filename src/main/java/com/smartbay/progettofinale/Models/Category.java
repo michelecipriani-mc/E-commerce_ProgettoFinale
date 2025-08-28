f6ab3301dfa17dbdd094619142256e87181d9992
Davide Latina <davide.latina616@gmail.com>
2025-08-28 18:07:26 +0200

    Commenti per documentazione pacchetto models

--- src/main/java/com/smartbay/progettofinale/Models/Category.java
+++ src/main/java/com/smartbay/progettofinale/Models/Category.java
@@ -8,6 +8,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Entità di persistenza per le categorie.
+ * <p>
+ * Mappa i dati di una categoria alla tabella "categories" nel database.
+ */
 @Setter
 @Getter
 @NoArgsConstructor
@@ -15,15 +20,22 @@ import lombok.Setter;
 @Table(name = "categories")
 public class Category {
 
+    /** Identificativo univoco della categoria. */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /** Nome della categoria. */
     @Column(nullable = false, length = 100)
     @NotEmpty
     @Size(max = 50)
     private String name;
 
+    /**
+     * Lista di articoli associati a questa categoria.
+     * <p>
+     * Relazione uno-a-molti: una categoria può avere più articoli.
+     */
     @OneToMany(mappedBy = "category")
     private List<Article> articles = new ArrayList<Article>();
     
